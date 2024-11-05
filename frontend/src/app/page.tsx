7cb824e0e6b459f69d5f950cb548c6a43ec9b992
MohamedElKasimi <mohamed0elkasimi@gmail.com>
2024-11-05 13:57:11 +0100

    Initialized project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <header className="w-full bg-blue-900 p-4 flex font-itim">
+        hello
+
+      </header>
+    </div>
+  );
+}
