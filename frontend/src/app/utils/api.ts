26945d8c9022d2db15b417b8de65dfdab3ebe681
MohamedElKasimi <mohamed0elkasimi@gmail.com>
2024-11-29 20:53:02 +0100

    Added .env for hosts

--- frontend/src/app/utils/api.ts
+++ frontend/src/app/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: process.env.NEXT_PUBLIC_HOST
 });
 
 export const setAuthToken =(token: string | null) => {
